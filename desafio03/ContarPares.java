e090ac9e4337ec3ae78037dda77b07ca5bc4bb8b
antikevin <Kevin_3467@yahoo.com>
2023-07-11 23:16:45 -0300

    feat: :tada: Desafio 3

---
+++ desafio03/ContarPares.java
@@ -0,0 +1,37 @@
+package desafio03;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ContarPares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        int count = conteParesComDiferencaK(arr, k);
+        System.out.println(count);
+    }
+
+    public static int conteParesComDiferencaK(int[] arr, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        int count = 0;
+        for (int num : arr) {
+            int complement = num + k;
+            if (frequencyMap.containsKey(complement)) {
+                count += frequencyMap.get(complement);
+            }
+        }
+
+        return count;
+    }
+}
